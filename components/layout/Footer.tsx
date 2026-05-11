a5e2bb301aa229c76eead01acac4f629b5592d31
muramatsu-jbn <muramatsu@jibun-note.co.jp>
2026-05-11 17:14:22 +0900

    メニュー変更

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -2,9 +2,9 @@ import Link from "next/link";
 
 const sitemapItems = [
 	{ label: "サービス", href: "/#scheme-cards" },
-	{ label: "re-born", href: "/#reborn" },
-	{ label: "business re-born", href: "/#businessReborn" },
+	{ label: "business re-born(法人のオーナー様)", href: "/#businessReborn" },
 	{ label: "不動産売却計画書＋オークション", href: "/#salePlan" },
+	{ label: "re-born(個人版)", href: "/#reborn" },
 ];
 
 export default function Footer() {
